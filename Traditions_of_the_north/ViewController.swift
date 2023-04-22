f0ece3addf93f6e17fa94dec4d22b5c828c56093
Usoltsev Alexander <Usoltsev92@icloud.com>
2023-04-22 18:19:07 +0700

    Initial Commit

---
+++ Traditions_of_the_north/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traditions_of_the_north
+//
+//  Created by Александр Усольцев on 22.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
