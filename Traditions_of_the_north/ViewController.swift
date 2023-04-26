818287ac45e035523832491dc512e9b5193bca57
Usoltsev Alexander <Usoltsev92@icloud.com>
2023-04-27 00:15:10 +0700

    доработка

--- Traditions_of_the_north/ViewController.swift
+++ Traditions_of_the_north/ViewController.swift
@@ -34,9 +34,13 @@ class ViewController: UIViewController {
        }
            
        let arrayHolidays = [
-           Holiday(name: "Halloween", dateComponents: DateComponents(month: 04, day: 26), greetingText: "Happy Halloween!", image: "halloween"),
-           Holiday(name: "New Year", dateComponents: DateComponents(month: 4, day: 27), greetingText: "Happy New Year!", image: "newyear"),
-           Holiday(name: "Valentine's Day", dateComponents: DateComponents(month: 4, day: 28), greetingText: "Happy Valentine's Day!", image: "valentine")
+           Holiday(name: "Всего лишь четверг", dateComponents: DateComponents(month: 04, day: 27), greetingText: "Но для тебя это может быть самым лучшим четвергом! ведь сегодня можно попить кофе и даже лечь пораньше поспать! прийти домой покушать и понимать что тренировки прошли не зря, ведь мышци ростут и каждый день ты становишься сильнее и лучшей версией себя а еще твое приложение работает с каждым днем все лучше и солнышко согревает землю. Скоро будет намного теплее и зеленее и вся серзость вокруг будет не такой уебанской как мы привыкли ее видеть и чем больше ты в россии тем больше ты привыкаешь к тому что ты в дерьме! и со временем ты забываешь как могут выглядеть номральные дороги, как могут выглядеть нормальные люди, что цены могут быть адекватные и соседи не срут в лифте и не тушат бычки в стенку подьезда. постарайся свалить ищ страны как можно скорее и заплатите ведьмаку чеканной монетой чеканнойй монетой чеканной монетой монетой монетой и мне тоже заплатите что б я уехал туда где можно жить лучше и счастливие что б моя женушка не болела и все в жизни сразу стало перкрасно , поскорей бы выучить свифт и бежать из блятской страны", image: "halloween"),
+           
+           
+           Holiday(name: "New Year", dateComponents: DateComponents(month: 5, day: 27), greetingText: "Happy New Year!", image: "newyear"),
+           
+           
+           Holiday(name: "Valentine's Day", dateComponents: DateComponents(month: 6, day: 28), greetingText: "Happy Valentine's Day!", image: "valentine")
        ]
            
        func setupHolidayGreeting(year: Int) {
